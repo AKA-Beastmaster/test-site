98a640df5990798e7f329503c0b45387c5cd0637
AKA-Beastmaster <ilhamnahian@gmail.com>
2021-09-19 09:47:35 -0500

    9-19

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,41 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Calculator';
+class calculator{
+    constructor(PreviousOperationtextelement, CurrentOperationtextelement) {
+        this.PreviousOperationtextelement = PreviousOperationtextelement
+        this.CurrentOperationtextelement = CurrentOperationtextelement
+        this.clear
+    }
+
+    clear(){
+        this.PreviousOperationtextelement = ''
+        this.CurrentOperationtextelement = ''
+        this.operation = undefined
+    }
+
+    delete(){
+
+    }
+
+    appendnumber(number){
+
+    }
+
+    chooseoperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updatedisplay(){
+
+    }
+}
+
+const numberbuttons = document.querySelectorAll('[data-number]')
+const operatorbuttons = document.querySelectorAll('[data-operator]')
+const clearbutton = document.querySelector('[data-clear]')
+const equalbutton = document.querySelector('[data-equal]')
+const delbutton = document.querySelector('[data-del]')
+const PreviousOperationtextelement = document.querySelector('[data-previous-operation]')
+const CurrentOperationtextelement = document.querySelector('[data-current-operation]')
